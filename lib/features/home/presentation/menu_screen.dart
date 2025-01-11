53026a1e041d9990fe9be85705a34776ea65bca4
tonygirgis <tonygirgis29@gmail.com>
2025-01-11 16:18:45 +0200

    add menu screen ui

--- lib/features/home/presentation/menu_screen.dart
+++ lib/features/home/presentation/menu_screen.dart
@@ -1,10 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:meal_monkey/core/utils/app_colors.dart';
+import 'package:meal_monkey/core/utils/constatns.dart';
+import 'package:meal_monkey/core/utils/font_styles.dart';
+import 'package:meal_monkey/features/home/presentation/widgets/menu_card.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            // app bar
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: AppConstants.defaultPadding32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Menu',
+                    style: FontsStyles.regular25,
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.shopping_cart,
+                        size: 22,
+                      ))
+                ],
+              ),
+            ),
+            // search bar
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: AppConstants.defaultPadding32),
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Icon(Icons.search),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8.0),
+                      child: Text('Search food'),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 60),
+            // categories card
+            Stack(
+                alignment: Alignment.centerLeft ,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.55,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                    color: AppColors.kPrimaryColor,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                  ),
+                ),
+                const Column(
+                children: [
+                  MenuCard(),
+                  SizedBox(height: 24),
+                  MenuCard(),
+                  SizedBox(height: 24),
+                  MenuCard(),
+                  SizedBox(height: 24),
+                  MenuCard(),
+                ],
+              ),
+
+
+              ]
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
---
+++ lib/features/home/presentation/widgets/menu_card.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:meal_monkey/core/utils/app_colors.dart';
+import 'package:meal_monkey/core/utils/font_styles.dart';
+
+class MenuCard extends StatelessWidget {
+  const MenuCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 59, right: 36),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 90,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
+                ),
+              ],
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 70),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Food',
+                    style: FontsStyles.regular25,
+                  ),
+                  Text(
+                    '120 Items',
+                    style: FontsStyles.regular12,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 25,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(alpha: 0.2), // Shadow color
+                  blurRadius: 10, // Spread of the shadow
+                  offset: const Offset(0, 4), // Position of the shadow
+                ),
+              ],
+            ),
+            child: CircleAvatar(
+              radius: 40,
+              backgroundColor: Colors.grey[200],
+              child: const Icon(
+                Icons.fastfood,
+                size: 50,
+              ),
+            ),
+          ),
+        ),
+
+        Positioned(
+          right: 15,
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_forward_ios,
+              color: AppColors.kPrimaryColor,
+            ),
+            style:  ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(Colors.white),
+              elevation: WidgetStateProperty.all(2),
+              shadowColor: WidgetStateProperty.all(Colors.grey),
+              shape: WidgetStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
